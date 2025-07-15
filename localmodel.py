a78ad09cdc0ebe2cb3360bdf19c70f98ecf88f80
Joep Elderman <joep@moeilijkedingen.nl>
2025-07-15 16:23:38 +0200

    added local model downloader and update gitignore

---
+++ localmodel.py
@@ -0,0 +1,30 @@
+import os
+from huggingface_hub import snapshot_download
+
+def modelprep():
+    hf_token = os.getenv("HUGGINGFACE_HUB_TOKEN")
+    if hf_token is None:
+        raise RuntimeError("HUGGINGFACE_HUB_TOKEN environment variable not set")
+
+    print("download snapshot Start")
+    snapshot_download('stabilityai/stable-diffusion-xl-base-1.0',
+                      local_dir='/Users/joep/freelance/nova/DiffusionLight/models',
+                      token=hf_token,
+                      resume_download=True)
+
+    snapshot_download('madebyollin/sdxl-vae-fp16-fix',
+                      local_dir='/Users/joep/freelance/nova/DiffusionLight/models',
+                      token=hf_token,
+                      resume_download=True)
+
+    snapshot_download('diffusers/controlnet-depth-sdxl-1.0',
+                      local_dir='/Users/joep/freelance/nova/DiffusionLight/models',
+                      token=hf_token,
+                      resume_download=True)
+
+    print("download snapshot done")
+
+if __name__ == '__main__':
+    print("Worker Start")
+    modelprep()
+    
