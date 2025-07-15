dccc35322573bc38e9d710189eb89038e74e791f
Joep Elderman <joep@moeilijkedingen.nl>
2025-07-15 17:45:24 +0200

    moved shared storage to root

--- relighting/argument.py
+++ relighting/argument.py
@@ -36,28 +36,28 @@ def get_control_signal_type(controlnet):
 
 
 SD_MODELS = {
-    "sd15_old": "/runpod-volume/models/stable-diffusion-inpainting",
-    "sd15_new": "/runpod-volume/models/stable-diffusion-inpainting",
-    "sd21": "/runpod-volume/models/stable-diffusion-2-inpainting",
-    "sdxl": "/runpod-volume/models/stable-diffusion-xl-base-1.0",
-    "sdxl_fast": "/runpod-volume/models/stable-diffusion-xl-base-1.0",
-    "sdxl_turbo": "/runpod-volume/models/sdxl-turbo",
-    "sd15_depth": "/runpod-volume/models/stable-diffusion-inpainting",
+    "sd15_old": "/runpod-volume/stable-diffusion-inpainting",
+    "sd15_new": "/runpod-volume/stable-diffusion-inpainting",
+    "sd21": "/runpod-volume/stable-diffusion-2-inpainting",
+    "sdxl": "/runpod-volume/stable-diffusion-xl-base-1.0",
+    "sdxl_fast": "/runpod-volume/stable-diffusion-xl-base-1.0",
+    "sdxl_turbo": "/runpod-volume/sdxl-turbo",
+    "sd15_depth": "/runpod-volume/stable-diffusion-inpainting",
 }
 
 VAE_MODELS = {
-    "sdxl": "/runpod-volume/models/sdxl-vae-fp16-fix",
-    "sdxl_fast": "/runpod-volume/models/sdxl-vae-fp16-fix",
+    "sdxl": "/runpod-volume/sdxl-vae-fp16-fix",
+    "sdxl_fast": "/runpod-volume/sdxl-vae-fp16-fix",
 }
 
 CONTROLNET_MODELS = {
-    "sd15_old": "/runpod-volume/models/stable-diffusion-v1-5-controlnet-normal",
-    "sd15_new": "/runpod-volume/models/control_v11p_sd15_normalbae",
-    "sd21": "/runpod-volume/models/controlnet-sd21-normalbae-diffusers",
-    "sdxl": "/runpod-volume/models/controlnet-depth-sdxl-1.0",
-    "sdxl_fast": "/runpod-volume/models/controlnet-depth-sdxl-1.0-small",
-    "sdxl_turbo": "/runpod-volume/models/controlnet-depth-sdxl-1.0-small",
-    "sd15_depth": "/runpod-volume/models/control_v11f1p_sd15_depth",
+    "sd15_old": "/runpod-volume/stable-diffusion-v1-5-controlnet-normal",
+    "sd15_new": "/runpod-volume/control_v11p_sd15_normalbae",
+    "sd21": "/runpod-volume/controlnet-sd21-normalbae-diffusers",
+    "sdxl": "/runpod-volume/controlnet-depth-sdxl-1.0",
+    "sdxl_fast": "/runpod-volume/controlnet-depth-sdxl-1.0-small",
+    "sdxl_turbo": "/runpod-volume/controlnet-depth-sdxl-1.0-small",
+    "sd15_depth": "/runpod-volume/control_v11f1p_sd15_depth",
 }
 
 SAMPLERS = {
