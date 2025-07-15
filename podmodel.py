7e98902e7524b5b04ff95f33472b226fd0a176b2
Joep Elderman <joep@moeilijkedingen.nl>
2025-07-15 21:24:17 +0200

    force download

--- podmodel.py
+++ podmodel.py
@@ -10,17 +10,20 @@ def modelprep():
     snapshot_download('stabilityai/stable-diffusion-xl-base-1.0',
                       local_dir='/workspace/stable-diffusion-xl-base-1.0',
                       token=hf_token,
-                      resume_download=True)
+                      force_download=True,
+                      resume_download=False)
 
     snapshot_download('madebyollin/sdxl-vae-fp16-fix',
                       local_dir='/workspace/sdxl-vae-fp16-fix',
                       token=hf_token,
-                      resume_download=True)
+                      force_download=True,
+                      resume_download=False)
 
     snapshot_download('diffusers/controlnet-depth-sdxl-1.0',
                       local_dir='/workspace/controlnet-depth-sdxl-1.0',
                       token=hf_token,
-                      resume_download=True)
+                      force_download=True,
+                      resume_download=False)
 
     print("download snapshot done")
 
