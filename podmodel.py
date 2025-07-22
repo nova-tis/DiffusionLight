f13618535378d9caea4784ed37854c737f64341a
Joep Elderman <joep@moeilijkedingen.nl>
2025-07-22 16:26:09 +0200

    updated runner for docker

--- podmodel.py
+++ podmodel.py
@@ -10,22 +10,22 @@ def modelprep():
     snapshot_download('stabilityai/stable-diffusion-xl-base-1.0',
                       local_dir='/data/models/stable-diffusion-xl-base-1.0',
                       token=hf_token,
-                      force_download=True,
-                      resume_download=False)
-
+                      force_download=False,
+                      resume_download=True)
+    print("download snapshot 1 done")
     snapshot_download('madebyollin/sdxl-vae-fp16-fix',
                       local_dir='/data/models/sdxl-vae-fp16-fix',
                       token=hf_token,
-                      force_download=True,
-                      resume_download=False)
-
+                      force_download=False,
+                      resume_download=True)
+    print("download snapshot 2 done")
     snapshot_download('diffusers/controlnet-depth-sdxl-1.0',
                       local_dir='/data/models/controlnet-depth-sdxl-1.0',
                       token=hf_token,
-                      force_download=True,
-                      resume_download=False)
+                      force_download=False,
+                      resume_download=True)
 
-    print("download snapshot done")
+    print("download snapshot 3 done")
 
 if __name__ == '__main__':
     print("Worker Start")
